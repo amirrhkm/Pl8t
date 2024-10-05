e891159ad8c345ea637855f3966ffac10f492a67
amir.zaid <amir.zaid@silentmode.my>
2024-10-05 23:53:11 +0800

    feat(staff): enums for employment type and positions

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -11,6 +11,17 @@ class Staff extends Model
 
     protected $fillable = ['name', 'nickname', 'employment_type', 'position', 'rate'];
 
+    const EMPLOYMENT_TYPES = [
+        'part_time' => 'Part-time',
+        'full_time' => 'Full-time',
+    ];
+
+    const POSITIONS = [
+        'bar' => 'Bar',
+        'kitchen' => 'Kitchen',
+        'flexible' => 'Bar/Kitchen',
+    ];
+
     public function shifts()
     {
         return $this->hasMany(Shift::class);
@@ -19,5 +30,15 @@ class Staff extends Model
     public function salaries()
     {
         return $this->hasMany(Salary::class);
+    }
+
+    public function formattedEmploymentType()
+    {
+        return self::EMPLOYMENT_TYPES[$this->employment_type] ?? $this->employment_type;
+    }
+
+    public function formattedPosition()
+    {
+        return self::POSITIONS[$this->position] ?? $this->position;
     }
 }
