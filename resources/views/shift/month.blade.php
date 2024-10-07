57cee11d54fe43443a08ae4b05820c621361264c
amir.zaid <amir.zaid@silentmode.my>
2024-10-07 23:43:17 +0800

    feat(shift): staff working hours groupBy(date) in current month

---
+++ resources/views/shift/month.blade.php
@@ -0,0 +1,34 @@
+<x-layout>
+    <x-slot:heading>Shifts for {{ Carbon\Carbon::create($year, $month, 1)->format('F Y') }}</x-slot:heading>
+
+    <div class="overflow-x-auto">
+        <table class="min-w-full bg-white">
+            <thead class="bg-gray-100">
+                <tr>
+                    <th class="py-2 px-4 border-b text-left">Date</th>
+                    <th class="py-2 px-4 border-b text-left">Staff Working Hours</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($shifts as $date => $dayShifts)
+                    <tr>
+                        <td class="py-2 px-4 border-b align-top">{{ $date }}</td>
+                        <td class="py-2 px-4 border-b">
+                            @foreach ($dayShifts as $shift)
+                                <div class="mb-2">
+                                    @if ($shift->staff)
+                                        <strong>{{ $shift->staff->name }} ({{ $shift->staff->nickname }}):</strong>
+                                    @else
+                                        <strong>Unknown Staff:</strong>
+                                    @endif
+                                    {{ Carbon\Carbon::parse($shift->start_time)->format('H:i') }} - 
+                                    {{ Carbon\Carbon::parse($shift->end_time)->format('H:i') }}
+                                </div>
+                            @endforeach
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-layout>
