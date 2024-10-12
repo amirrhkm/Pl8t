222a4ebc67c3205cc49eb456af59d1de0be1e4db
amir.zaid <amir.zaid@silentmode.my>
2024-10-12 18:16:22 +0800

    feat(shift): init dates in all month

---
+++ database/factories/ShiftFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Shift;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+class ShiftFactory extends Factory
+{
+    protected $model = Shift::class;
+
+    public function definition()
+    {
+        static $date = '2024-01-01';
+
+        return [
+            'staff_id' => 1,
+            'date' => $date,
+            'start_time' => "07:30:00"->format('H:i'),
+            'end_time' => "16:00:00"->format('H:i'),
+            'total_hours' => 7.5,
+            'overtime_hours' => 0,
+            'is_public_holiday' => false,
+        ];
+    }
+
+    public function incrementDate()
+    {
+        return $this->state(function (array $attributes) {
+            $currentDate = Carbon::parse($attributes['date']);
+            $nextDate = $currentDate->addDay()->format('Y-m-d');
+            return ['date' => $nextDate];
+        });
+    }
+}
