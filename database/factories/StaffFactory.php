158a270c8ce0b20054c4cca715191f7a5bbb5925
amir.zaid <amir.zaid@silentmode.my>
2024-10-12 18:15:49 +0800

    feat(staff): insert factory for dataHandler

---
+++ database/factories/StaffFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class StaffFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => 1,
+            'name' => "admin",
+            'nickname' => "admin",
+            'employment_type' => "full_time",
+            'position' => "flexible",
+            'rate' => 999999
+        ];
+    }
+
+}
