0c38f14767ab071d2812eab90a99bf9e720454e6
amir.zaid <amir.zaid@silentmode.my>
2024-10-13 23:55:49 +0800

    fix(salary): var naming and salary calculation

--- resources/views/staff/shift.blade.php
+++ resources/views/staff/shift.blade.php
@@ -12,8 +12,10 @@
             $date = Carbon\Carbon::parse($yearMonth);
             $month = $date->format('m');
             $year = $date->format('Y');
-            $monthTotalHours = 0;
-            $monthOvertimeHours = 0;
+            $month_reg_hours = 0;
+            $month_reg_ot_hours = 0;
+            $month_ph_hours = 0;
+            $month_ph_ot_hours = 0;
         @endphp
 
         <h2 class="text-xl font-bold mb-4">{{ $date->format('F Y') }}</h2>
@@ -24,65 +26,94 @@
                         <th class="py-2 px-4 border-b text-left">Date</th>
                         <th class="py-2 px-4 border-b text-left">Start Time</th>
                         <th class="py-2 px-4 border-b text-left">End Time</th>
-                        <th class="py-2 px-4 border-b text-left">Total Hours</th>
-                        <th class="py-2 px-4 border-b text-left">Overtime Hours</th>
+                        <th class="py-2 px-4 border-b text-left">Regular Hours</th>
+                        <th class="py-2 px-4 border-b text-left">Regular OT Hours</th>
+                        <th class="py-2 px-4 border-b text-left">PH Hours</th>
+                        <th class="py-2 px-4 border-b text-left">PH OT Hours</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($monthShifts as $shift)
                         @php
-                            $totalHours = $shift->start_time->diffInHours($shift->end_time) - $shift->break_duration;
-                            $overtimeHours = max(0, $totalHours - 8);
-                            $monthTotalHours += $totalHours;
-                            $monthOvertimeHours += $overtimeHours;
+                            $hours = $shift->start_time->diffInHours($shift->end_time) - $shift->break_duration;
+                            $otHours = max(0, $hours - 8);
+
+                            $reg_hours = 0;
+                            $reg_ot_hours = 0;
+                            $ph_hours = 0;
+                            $ph_ot_hours = 0;
+
+                            if ($shift->is_public_holiday) {
+                                $ph_hours = $hours;
+                                $ph_ot_hours = $otHours;
+                                $month_ph_hours += $ph_hours;
+                                $month_ph_ot_hours += $ph_ot_hours;
+                            } else {
+                                $reg_hours = $hours;
+                                $reg_ot_hours = $otHours;
+                                $month_reg_hours += $reg_hours;
+                                $month_reg_ot_hours += $reg_ot_hours;
+                            }
                         @endphp
                         <tr>
-                            <td class="py-2 px-4 border-b">{{ Carbon\Carbon::parse($shift->date)->format('Y-m-d') }}</td>
+                            <td class="py-2 px-4 border-b">{{ Carbon\Carbon::parse($shift->date)->format('d/m') }}</td>
                             <td class="py-2 px-4 border-b">{{ $shift->start_time->format('H:i') }}</td>
                             <td class="py-2 px-4 border-b">{{ $shift->end_time->format('H:i') }}</td>
-                            <td class="py-2 px-4 border-b">{{ $totalHours }}</td>
-                            <td class="py-2 px-4 border-b">{{ $overtimeHours }}</td>
+                            <td class="py-2 px-4 border-b">{{ $reg_hours }}</td>
+                            <td class="py-2 px-4 border-b">{{ $reg_ot_hours }}</td>
+                            <td class="py-2 px-4 border-b">{{ $ph_hours }}</td>
+                            <td class="py-2 px-4 border-b">{{ $ph_ot_hours }}</td>
                         </tr>
                     @endforeach
                 </tbody>
                 <tfoot class="bg-gray-100">
-                    @php
-                        $regularPay = $monthTotalHours * $staff->rate;
-                        $overtimePay = $monthOvertimeHours * 10;
-                        $totalSalary = $regularPay + $overtimePay;
-                        
-                        $salary = \App\Models\Salary::updateOrCreate(
-                            ['staff_id' => $staff->id, 'month' => $month, 'year' => $year],
-                            [
-                                'total_hours' => $monthTotalHours,
-                                'total_overtime_hours' => $monthOvertimeHours,
-                                'total_salary' => $totalSalary,
-                                'total_public_holiday_hours' => 0,
-                            ]
-                        );
-                    @endphp
-                    <tr>
-                        <td colspan="3" class="py-2 px-4 border-b font-bold text-right">Month Totals:</td>
-                        <td class="py-2 px-4 border-b font-bold">{{ $monthTotalHours }}</td>
-                        <td class="py-2 px-4 border-b font-bold">{{ $monthOvertimeHours }}</td>
-                    </tr>
-                    <tr>
-                        <td colspan="4" class="py-2 px-4 border-b font-bold text-right">Regular Pay:</td>
-                        <td class="py-2 px-4 border-b font-bold">RM {{ number_format($regularPay, 2) }}</td>
-                    </tr>
-                    <tr>
-                        <td colspan="4" class="py-2 px-4 border-b font-bold text-right">Overtime Pay:</td>
-                        <td class="py-2 px-4 border-b font-bold">RM {{ number_format($overtimePay, 2) }}</td>
-                    </tr>
-                    <tr>
-                        <td colspan="4" class="py-2 px-4 border-b font-bold text-right">Public Holiday Pay:</td>
-                        <td class="py-2 px-4 border-b font-bold">RM (To-Be-Implemented)</td>
-                    </tr>
-                    <tr>
-                        <td colspan="4" class="py-2 px-4 border-b font-bold text-right">Total Salary:</td>
-                        <td class="py-2 px-4 border-b font-bold">RM {{ number_format($totalSalary, 2) }}</td>
-                    </tr>
-                </tfoot>
+                @php
+                    $reg_pay = $month_reg_hours * $staff->rate;
+                    $reg_ot_pay = $month_reg_ot_hours * 10;
+                    $ph_pay = $month_ph_hours * $staff->rate * 2;
+                    $ph_ot_pay = $month_ph_ot_hours * 10 * 2;
+                    
+                    $total_salary = $reg_pay + $reg_ot_pay + $ph_pay + $ph_ot_pay;
+                    
+                    $salary = \App\Models\Salary::updateOrCreate(
+                        ['staff_id' => $staff->id, 'month' => $month, 'year' => $year],
+                        [
+                            'total_reg_hours' => $month_reg_hours,
+                            'total_reg_ot_hours' => $month_reg_ot_hours,
+                            'total_ph_hours' => $month_ph_hours,
+                            'total_ph_ot_hours' => $month_ph_ot_hours,
+                            'total_salary' => $total_salary,
+                        ]
+                    );
+                @endphp
+                <tr>
+                    <td colspan="3" class="py-2 px-4 border-b font-bold text-right"></td>
+                    <td class="py-2 px-4 border-b font-bold">{{ $month_reg_hours }}</td>
+                    <td class="py-2 px-4 border-b font-bold">{{ $month_reg_ot_hours }}</td>
+                    <td class="py-2 px-4 border-b font-bold">{{ $month_ph_hours }}</td>
+                    <td class="py-2 px-4 border-b font-bold">{{ $month_ph_ot_hours }}</td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="py-2 px-4 text-right">Regular Pay:</td>
+                    <td class="py-2 px-4">RM {{ number_format($reg_pay, 2) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="py-2 px-4 text-right">Regular Overtime Pay:</td>
+                    <td class="py-2 px-4">RM {{ number_format($reg_ot_pay, 2) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="py-2 px-4 text-right">Public Holiday Pay:</td>
+                    <td class="py-2 px-4">RM {{ number_format($ph_pay, 2) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="py-2 px-4 text-right">Public Holiday Overtime Pay:</td>
+                    <td class="py-2 px-4">RM {{ number_format($ph_ot_pay, 2) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="6" class="py-2 px-4 border-b font-bold text-right">Total Salary:</td>
+                    <td class="py-2 px-4 border-b font-bold">RM {{ number_format($total_salary, 2) }}</td>
+                </tr>
+            </tfoot>
             </table>
         </div>
     @endforeach
