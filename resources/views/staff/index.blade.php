028e5739c8c5f0b46991c2767a4ca817836ef91f
amir.zaid <amir.zaid@silentmode.my>
2024-10-15 20:32:25 +0800

    fix: button to add new staff

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -84,4 +84,10 @@
             </tbody>
         </table>
     </div>
+
+    <div class="mt-8 flex justify-center">
+       <a href="{{ route('staff.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+           Create New Staff
+       </a>
+   </div>
 </x-layout>
