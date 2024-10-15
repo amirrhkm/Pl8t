f4816d368a6eb6c39d97f2dd2f2e082737d4886a
amir.zaid <amir.zaid@silentmode.my>
2024-10-16 01:07:23 +0800

    chore(staff): center text

--- resources/views/staff/wildcard.blade.php
+++ resources/views/staff/wildcard.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <x-slot:heading>Shift Summary for {{ $staff->name }}</x-slot:heading>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div class="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         @php
             $currentYear = now()->year;
             $months = collect(range(1, 12))->map(function($month) use ($currentYear) {
@@ -18,9 +18,9 @@
             @endphp
             <a href="{{ route('staff.shift', ['staff' => $staff->id, 'year' => $date->year, 'month' => $date->month]) }}" 
                class="block p-6 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $date->format('F Y') }}</h5>
-                <p class="font-normal text-gray-700">Total Hours: {{ number_format($totalHours, 1) }}</p>
-                <p class="font-normal text-gray-700">Salary: RM {{ number_format($totalSalary, 2) }}</p>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 text-center">{{ $date->format('F Y') }}</h5>
+                <p class="font-normal text-gray-700 text-center">Total Hours: {{ number_format($totalHours, 1) }}</p>
+                <p class="font-normal text-gray-700 text-center">Salary: RM {{ number_format($totalSalary, 2) }}</p>
             </a>
         @endforeach
     </div>
