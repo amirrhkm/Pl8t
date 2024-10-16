1ce58d88a2555fe0a81d894546eb5bd8d6aa67b1
amir.zaid <amir.zaid@silentmode.my>
2024-10-17 07:00:14 +0800

    feat(auth): routes to login/register

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaffController;
 use App\Http\Controllers\ShiftController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+    ->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])
+    ->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
 
 //Home Routes
 Route::get('/', [HomeController::class, 'index'])
