f95619137ac68c1d0c0db4600cf812345b28b956
amir.zaid <amir.zaid@silentmode.my>
2024-10-18 18:15:35 +0800

    fix(login): use username instead of email

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
-    <x-slot name="heading">Login</x-slot>
-    <x-slot name="description">Welcome back! Please login to your account.</x-slot>
+    <x-slot name="heading">Welcome back to Tally Up!</x-slot>
+    <x-slot name="description">Please login to your account.</x-slot>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form class="space-y-6" action="{{ route('login') }}" method="POST">
                 @csrf
                 <div>
-                    <label for="email" class="block text-sm font-medium text-gray-700">Email address</label>
+                    <label for="name" class="block text-sm font-medium text-gray-700">Username</label>
                     <div class="mt-1">
-                        <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <input id="name" name="name" type="text" autocomplete="name" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                     </div>
                 </div>
 
