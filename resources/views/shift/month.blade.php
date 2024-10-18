7b0fc32774de302bd538948129c45e04a1c54c90
amir.zaid <amir.zaid@silentmode.my>
2024-10-18 18:20:24 +0800

    chore(shift): fix styling and include no shifts prompt

--- resources/views/shift/month.blade.php
+++ resources/views/shift/month.blade.php
@@ -8,9 +8,9 @@
                 <thead class="bg-gray-100">
                     <tr>   
                         <th class="py-2 px-4 border-b text-left">Date</th>
-                        <th class="py-2 px-4 border-b text-left">Opening</th>
-                        <th class="py-2 px-4 border-b text-left">Closing</th>
-                        <th class="py-2 px-4 border-b text-left">OT</th>
+                        <th class="py-2 px-4 border-b text-left text-center">Opening</th>
+                        <th class="py-2 px-4 border-b text-left text-center">Closing</th>
+                        <th class="py-2 px-4 border-b text-left text-center">OT</th>
                         <th class="py-2 px-4 border-b text-right"></th>
                     </tr>
                 </thead>
@@ -19,7 +19,7 @@
                         <tr>
                             <td class="py-2 px-4 border-b align-top">{{ Carbon\Carbon::parse($date)->format('d') }}</td>
                             <td class="py-2 px-4 border-b">
-                                <div class="flex flex-wrap gap-1">
+                                <div class="flex flex-wrap gap-1 justify-center">
                                     @foreach ($dayShifts as $shift)
                                         @if ($shift->staff && $shift->staff->name !== "admin" && Carbon\Carbon::parse($shift->start_time)->format('H:i') < '12:00')
                                             <span class="bg-green-100 px-2 py-1 rounded text-sm">
@@ -30,7 +30,12 @@
                                 </div>
                             </td>
                             <td class="py-2 px-4 border-b">
-                                <div class="flex flex-wrap gap-1">
+                                <div class="flex flex-wrap gap-1 justify-center">
+                                    @if (count($dayShifts) == 1 && $dayShifts->first()->staff->name === "admin")
+                                        <span class="bg-white px-2 py-1 rounded text-sm text-gray-500 italic">
+                                            No shifts assigned for this day.
+                                        </span>
+                                    @endif
                                     @foreach ($dayShifts as $shift)
                                         @if ($shift->staff && $shift->staff->name !== "admin" && Carbon\Carbon::parse($shift->end_time)->format('H:i') == '23:00')
                                             <span class="bg-orange-100 px-2 py-1 rounded text-sm">
@@ -41,7 +46,7 @@
                                 </div>
                             </td>
                             <td class="py-2 px-4 border-b">
-                                <div class="flex flex-wrap gap-1">
+                                <div class="flex flex-wrap gap-1 justify-center">
                                     @foreach ($dayShifts as $shift)
                                         @php
                                             $hours = Carbon\Carbon::parse($shift->start_time)->diffInHours(Carbon\Carbon::parse($shift->end_time)) - $shift->break_duration;
@@ -55,7 +60,9 @@
                                 </div>
                             </td>
                             <td class="py-2 px-4 border-b text-right">
-                                <a href="{{ route('shift.details', ['date' => $date]) }}" class="text-blue-500 hover:underline">Details</a>
+                                <a href="{{ route('shift.details', ['date' => $date]) }}" class="text-blue-500 hover:underline inline-block">
+                                    <svg class="w-5 h-5 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                                </a>
                             </td>
                         </tr>
                     @endforeach
