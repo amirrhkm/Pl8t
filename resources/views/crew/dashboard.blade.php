4bb7166fca8b59e8afdb0451e10496f7e1f2f6f5
amir.zaid <amir.zaid@silentmode.my>
2024-10-20 00:18:36 +0800

    feat(crew): button to edit account and logout

--- resources/views/crew/dashboard.blade.php
+++ resources/views/crew/dashboard.blade.php
@@ -22,7 +22,7 @@
     <body class="h-full">
         <header class="bg-transparent">
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-4xl font-bold text-white text-center drop-shadow-lg">Tally Up</h1>
+                <h1 class="text-4xl font-bold text-white text-center drop-shadow-lg pt-10">Tally Up</h1>
             </div>
         </header>
         
@@ -32,7 +32,25 @@
                     <div class="bg-gradient-to-br from-indigo-50 to-blue-100 p-8 rounded-xl shadow-lg">
                         <div class="flex justify-between items-center mb-6">
                             <h2 class="text-3xl font-bold text-indigo-800">{{ $staff->name }}'s Dashboard</h2>
-                            <p class="text-lg font-semibold text-gray-600">{{ now()->format('d M Y (l)') }}</p>
+                            <div class="flex items-center">
+                                <p class="text-lg font-semibold text-gray-600 mr-2">{{ now()->format('d M Y (l)') }}</p>
+                                <div class="flex items-center">
+                                    <a href="{{ route('account.settings') }}" class="text-indigo-500 hover:text-indigo-600 mr-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                        </svg>
+                                    </a>
+                                    <form action="{{ route('logout') }}" method="POST" class="inline-flex items-center">
+                                        @csrf
+                                        <button type="submit" class="text-red-500 hover:text-red-600">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
         
                         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
@@ -54,7 +72,7 @@
                                 </p>
                             </div>
                             <div class="bg-white p-6 rounded-lg shadow-md transition duration-300 ease-in-out hover:shadow-xl flex flex-col items-center justify-center">
-                                <h3 class="text-lg font-semibold mb-2 text-gray-600">Previous Salaries</h3>
+                                <h3 class="text-lg font-semibold mb-2 text-indigo-600">Previous Salaries</h3>
                                 <div class="text-center mb-2">
                                     <p class="text-md font-semibold text-gray-700">{{ $lastMonthName }}: RM {{ number_format($last_month_salary, 2) }}</p>
                                     <p class="text-sm {{ $percentage_diff_last_month >= 0 ? 'text-green-600' : 'text-red-600' }}">
@@ -154,7 +172,7 @@
                                                                             <span class="w-1.5 h-1.5 bg-indigo-500 rounded-full mr-1.5"></span>
                                                                             {{ $shift->staff->name }}
                                                                         </span>
-                                                                        <span class="ml-2 text-xs {{ $bgColor }} {{ $textColor }} px-1.5 py-0.5 rounded-full">
+                                                                        <span class="ml-2 text-md {{ $bgColor }} {{ $textColor }} px-1.5 py-0.5 rounded-full">
                                                                             {{ $startTime }}-{{ $endTime }}
                                                                         </span>
                                                                     </li>
