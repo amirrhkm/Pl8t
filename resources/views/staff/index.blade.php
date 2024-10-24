5a83bbe84cdc084cbd153608efb81154562fb766
amir.zaid <amir.zaid@silentmode.my>
2024-10-24 16:40:12 +0800

    feat(staff): button to staff leave dashboard

--- resources/views/staff/index.blade.php
+++ resources/views/staff/index.blade.php
@@ -31,6 +31,9 @@
                                     <td class="py-2 px-4 text-center">{{ $member->formattedPosition() }}</td>
                                     <td class="py-2 px-4">
                                         <div class="flex justify-end space-x-2">
+                                            <a href="{{ route('staff.leave', $member->id) }}" class="text-purple-600 hover:text-purple-800">
+                                                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                                            </a>
                                             <a href="{{ route('staff.wildcard', $member->id) }}" class="text-green-600 hover:text-green-800">
                                                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
                                             </a>
