2cbba6767d144d4eeba3f75824f2c9c4c8dd77b8
amir.zaid <amir.zaid@silentmode.my>
2024-10-28 13:49:16 +0800

    feat(seeder): seed salesDaily and Shift to 2025

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,12 @@ namespace Database\Seeders;
 use App\Models\Staff;
 use App\Models\Shift;
 use App\Models\User;
+use App\Models\SalesCumulative;
+use App\Models\SalesDaily;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -21,6 +24,21 @@ class DatabaseSeeder extends Seeder
             ->sequence(fn ($sequence) => ['date' => Carbon::createFromFormat('Y-m-d', '2024-01-01')
             ->addDays($sequence->index)->format('Y-m-d')])
             ->create();
+        Shift::factory()
+            ->count(365) //2025
+            ->sequence(fn ($sequence) => ['date' => Carbon::createFromFormat('Y-m-d', '2025-01-01')
+            ->addDays($sequence->index)->format('Y-m-d')])
+            ->create();
         User::factory()->count(1)->create();
+        SalesDaily::factory()
+            ->count(366) //2024 is a Leap Year
+            ->sequence(fn ($sequence) => ['date' => Carbon::createFromFormat('Y-m-d', '2024-01-01')
+            ->addDays($sequence->index)->format('Y-m-d')])
+            ->create();
+        SalesDaily::factory()
+            ->count(365) //2025
+            ->sequence(fn ($sequence) => ['date' => Carbon::createFromFormat('Y-m-d', '2025-01-01')
+            ->addDays($sequence->index)->format('Y-m-d')])
+            ->create();
     }
 }
