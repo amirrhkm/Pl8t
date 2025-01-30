9ca4120d40e74ecfccc226800b3e1c2ec37b7ff2
amir.zaid <amir.zaid@silentmode.my>
2025-01-31 00:00:34 +0800

    chore(holidays): restructure styling for selected

--- resources/views/holidays/index.blade.php
+++ resources/views/holidays/index.blade.php
@@ -54,13 +54,16 @@
 
         <div class="mt-6 bg-white p-6 rounded-lg shadow-md">
             <h3 class="text-xl font-semibold mb-4 text-gray-800">Selected Public Holidays</h3>
-            <ul id="selectedHolidays" class="space-y-2">
+            <div id="selectedHolidays" class="flex flex-wrap gap-2">
                 @foreach($holidays as $holiday)
-                    <li class="flex items-center bg-green-600 p-3 rounded-lg justify-center">
-                        <span class="text-white font-semibold">{{ \Carbon\Carbon::parse($holiday)->format('d M Y (D)') }}</span>
+                    <li class="flex items-center bg-green-50 px-3 py-2 rounded-lg border border-green-200 text-sm">
+                        <svg class="w-4 h-4 text-green-600 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                        </svg>
+                        <span class="text-green-800 font-medium">{{ \Carbon\Carbon::parse($holiday)->format('d M Y (D)') }}</span>
                     </li>
                 @endforeach
-            </ul>
+            </div>
         </div>
     </div>
 
